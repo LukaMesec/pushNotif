e4ffc3bae17634167209c3781592062e13474ca2
LukaMesec <luka@mesec.org>
2019-02-21 09:16:40 +0100

    push

---
+++ build/client/precache-manifest.bdc6a11874e8075a8daf3d776d4d8582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d064a436e7ea078a80f9",
+    "url": "/static/js/main.d064a436.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "d064a436e7ea078a80f9",
+    "url": "/static/css/main.4cdc5eb9.chunk.css"
+  },
+  {
+    "revision": "264d443fd697c23e530f0d5e0c732caa",
+    "url": "/index.html"
+  }
+];
